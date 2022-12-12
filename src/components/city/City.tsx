a2ae0b2d5db9116725a79715bb3cfc48ed42e418
Reinaldo Leal <reinaldoleal@gmail.com>
2022-12-12 09:03:19 -0300

    ajustes de layout

--- src/components/city/City.tsx
+++ src/components/city/City.tsx
@@ -65,127 +65,129 @@ function City({ ...props }: PropsCity) {
           className="nav-icon"
         />
       </div>
-      {weather.name && (
-        <div key={weather.id}>
-          <div className="weather">
-            <div>
-              <p>{weather.name.toUpperCase()}</p>
-              <span>{weather.weather?.[0].description}</span>
-            </div>
-          </div>
-          <div className="temp">
-            <div className="temp-now">
-              <span>{Math.trunc(weather.main?.temp || 0)}</span>
-            </div>
-            <div className="temp-now-others">
-              <div className="temp-now-others-ground">
-                <p>&deg;C</p>
+      <div className="container-weather-center">
+        {weather.name && (
+          <div key={weather.id}>
+            <div className="weather">
+              <div>
+                <p>{weather.name.toUpperCase()}</p>
+                <span>{weather.weather?.[0].description}</span>
               </div>
-              <div className="temp-now-others-max">
-                <div className="temp-now-others-icon">
-                  <FontAwesomeIcon icon={faArrowUp} />
+            </div>
+            <div className="temp">
+              <div className="temp-now">
+                <span>{Math.trunc(weather.main?.temp || 0)}</span>
+              </div>
+              <div className="temp-now-others">
+                <div className="temp-now-others-ground">
+                  <p>&deg;C</p>
                 </div>
-                <div className="temp-now-others-font">
-                  <p>{Math.trunc(weather.main?.temp_max || 0)}&deg;</p>
+                <div className="temp-now-others-max">
+                  <div className="temp-now-others-icon">
+                    <FontAwesomeIcon icon={faArrowUp} />
+                  </div>
+                  <div className="temp-now-others-font">
+                    <p>{Math.trunc(weather.main?.temp_max || 0)}&deg;</p>
+                  </div>
+                </div>
+                <div className="temp-now-others-min">
+                  <div className="temp-now-others-icon">
+                    <FontAwesomeIcon icon={faArrowDown} />
+                  </div>
+                  <div className="temp-now-others-font">
+                    <p>{Math.trunc(weather.main?.temp_min || 0)}&deg;</p>
+                  </div>
                 </div>
               </div>
-              <div className="temp-now-others-min">
-                <div className="temp-now-others-icon">
-                  <FontAwesomeIcon icon={faArrowDown} />
+            </div>
+            <div className="temp-img">
+              <img
+                src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@2x.png`}
+                alt="Weather"
+              />
+            </div>
+            <div className="forcast">
+              <div className="forcast-time">
+                <div>
+                  <span>dawn</span>
                 </div>
-                <div className="temp-now-others-font">
-                  <p>{Math.trunc(weather.main?.temp_min || 0)}&deg;</p>
+                <div>
+                  <img
+                    src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}.png`}
+                    alt="Weather"
+                  />
+                </div>
+                <div>
+                  <p>13&deg; C</p>
+                </div>
+              </div>
+              <div className="forcast-time">
+                <div>
+                  <span>morning</span>
+                </div>
+                <div>
+                  <img
+                    src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}.png`}
+                    alt="Weather"
+                  />
+                </div>
+                <div>
+                  <p>15&deg; C</p>
+                </div>
+              </div>
+              <div className="forcast-time">
+                <div>
+                  <span>afternoon</span>
+                </div>
+                <div>
+                  <img
+                    src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}.png`}
+                    alt="Weather"
+                  />
+                </div>
+                <div>
+                  <p>15&deg; C</p>
+                </div>
+              </div>
+              <div className="forcast-time">
+                <div>
+                  <span>night</span>
+                </div>
+                <div>
+                  <img
+                    src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}.png`}
+                    alt="Weather"
+                  />
+                </div>
+                <div>
+                  <p>15&deg; C</p>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="temp-img">
-            <img
-              src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@2x.png`}
-              alt="Weather"
-            />
-          </div>
-          <div className="forcast">
-            <div className="forcast-time">
-              <div>
-                <span>dawn</span>
+            <div className="others">
+              <div className="others-grid">
+                <p>wind speed</p>
+                <span>{weather?.wind?.speed}m/s</span>
               </div>
-              <div>
-                <img
-                  src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}.png`}
-                  alt="Weather"
-                />
+              <hr className={styleContainer} />
+              <div className="others-grid">
+                <p>sunrise</p>
+                <span>{format(weather?.sys?.sunrise || 0, 'HH:MM')} AM</span>
               </div>
-              <div>
-                <p>13&deg; C</p>
+              <hr className={styleContainer} />
+              <div className="others-grid">
+                <p>sunset</p>
+                <span>{format(weather?.sys?.sunset || 0, 'HH:MM')} PM</span>
               </div>
-            </div>
-            <div className="forcast-time">
-              <div>
-                <span>morning</span>
-              </div>
-              <div>
-                <img
-                  src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}.png`}
-                  alt="Weather"
-                />
-              </div>
-              <div>
-                <p>15&deg; C</p>
-              </div>
-            </div>
-            <div className="forcast-time">
-              <div>
-                <span>afternoon</span>
-              </div>
-              <div>
-                <img
-                  src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}.png`}
-                  alt="Weather"
-                />
-              </div>
-              <div>
-                <p>15&deg; C</p>
-              </div>
-            </div>
-            <div className="forcast-time">
-              <div>
-                <span>night</span>
-              </div>
-              <div>
-                <img
-                  src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}.png`}
-                  alt="Weather"
-                />
-              </div>
-              <div>
-                <p>15&deg; C</p>
+              <hr className={styleContainer} />
+              <div className="others-grid">
+                <p>humidity</p>
+                <span>{weather?.main?.humidity}%</span>
               </div>
             </div>
           </div>
-          <div className="others">
-            <div className="others-grid">
-              <p>wind speed</p>
-              <span>{weather?.wind?.speed}m/s</span>
-            </div>
-            <hr className={styleContainer} />
-            <div className="others-grid">
-              <p>sunrise</p>
-              <span>{format(weather?.sys?.sunrise || 0, 'HH:MM')} AM</span>
-            </div>
-            <hr className={styleContainer} />
-            <div className="others-grid">
-              <p>sunset</p>
-              <span>{format(weather?.sys?.sunset || 0, 'HH:MM')} PM</span>
-            </div>
-            <hr className={styleContainer} />
-            <div className="others-grid">
-              <p>humidity</p>
-              <span>{weather?.main?.humidity}%</span>
-            </div>
-          </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
